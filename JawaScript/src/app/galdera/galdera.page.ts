b22094d14c021deb25d31985a91dead27346975e
unknown <Adminpremialocal@HP-PROBOOK-19.uni.lan>
2020-01-16 11:48:51 +0100

    10 preguntas

--- JawaScript/src/app/galdera/galdera.page.ts
+++ JawaScript/src/app/galdera/galdera.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { gald } from '../models/task.interface';
 import { TodogalderakService } from '../services/todogalderak.service'
+import { delay } from 'rxjs/operators';
 @Component({
   selector: 'app-galdera',
   templateUrl: './galdera.page.html',
@@ -9,8 +10,9 @@ import { TodogalderakService } from '../services/todogalderak.service'
 })
 export class GalderaPage implements OnInit {
 
-  galderak: gald[];
-  ids = [];
+  private galderak: gald[];
+  private ids = [];
+  private i: number;
   constructor(private galderakService: TodogalderakService) { }
 
   ngOnInit() {
@@ -18,45 +20,78 @@ export class GalderaPage implements OnInit {
       this.galderak = res;
     });
     (async () => {
-      await this.delay(1500);
+      await this.delay(2000);
+
+      var primerRandom = (Math.floor(Math.random() * this.galderak.length) + 1);
+      await this.randomOrderAnswer(primerRandom);
+
       console.log(this.galderak);
-      this.randomOrderAnswer();
     })();
   }
+
+  
 
   delay(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
 
-   randomOrderAnswer(){
-    
-     var galderaSlide = $(".galderaSlide");
-     var primerRandom = (Math.floor(Math.random()* this.galderak.length)+1);
-     console.log(this.galderak[primerRandom]);
-     this.ids.push(this.galderak[primerRandom].Galdera);
-     var html = '<b><p lines="inset" button > '+ this.galderak[primerRandom].Galdera +' </p></b>';
-     switch(Math.floor(Math.random() * 6)+1){
-       case 1:
-          html += '<ion-button lines="inset" button >'+this.galderak[primerRandom].Erantzun1+'</ion-button><ion-button lines="inset" button >'+this.galderak[primerRandom].Erantzun2+'</ion-button><ion-button lines="inset" button >'+this.galderak[primerRandom].ErantzunZuzena+'</ion-button>'
-         break;
-       case 2:
-        html += '<ion-button lines="inset" button >'+this.galderak[primerRandom].Erantzun1+'</ion-button><ion-button lines="inset" button >'+this.galderak[primerRandom].ErantzunZuzena+'</ion-button><ion-button lines="inset" button >'+this.galderak[primerRandom].Erantzun2+'</ion-button>'
-         break;
-       case 3:
-        html += '<ion-button lines="inset" button >'+this.galderak[primerRandom].Erantzun2+'</ion-button><ion-button lines="inset" button >'+this.galderak[primerRandom].Erantzun1+'</ion-button><ion-button lines="inset" button >'+this.galderak[primerRandom].ErantzunZuzena+'</ion-button>'
-         break;
-       case 4:
-        html += '<ion-button lines="inset" button >'+this.galderak[primerRandom].Erantzun2+'</ion-button><ion-button lines="inset" button >'+this.galderak[primerRandom].ErantzunZuzena+'</ion-button><ion-button lines="inset" button >'+this.galderak[primerRandom].Erantzun1+'</ion-button>'
-         break;
-       case 5:
-        html += '<ion-button lines="inset" button >'+this.galderak[primerRandom].ErantzunZuzena+'</ion-button><ion-button lines="inset" button >'+this.galderak[primerRandom].Erantzun1+'</ion-button><ion-button lines="inset" button >'+this.galderak[primerRandom].Erantzun2+'</ion-button>'
-         break;
-       case 6:
-        html += '<ion-button lines="inset" button >'+this.galderak[primerRandom].ErantzunZuzena+'</ion-button><ion-button lines="inset" button >'+this.galderak[primerRandom].Erantzun2+'</ion-button><ion-button lines="inset" button >'+this.galderak[primerRandom].Erantzun1+'</ion-button>'
-         break;
-     }
-     $(".galderaSlide").html(html);
-   }
+  randomOrderAnswer(primerRandom) {
+    this.ids.push(this.galderak[primerRandom].id);
+    console.log(this.ids);
+    document.getElementById("galderaP").textContent = this.galderak[primerRandom].Galdera;
+    switch (Math.floor(Math.random() * 6) + 1) {
+      case 1:
+        document.getElementById("option1btn").textContent = this.galderak[primerRandom].Erantzun1;
+        document.getElementById("option2btn").textContent = this.galderak[primerRandom].Erantzun2;
+        document.getElementById("option3btn").textContent = this.galderak[primerRandom].ErantzunZuzena;
+        break;
+      case 2:
+        document.getElementById("option1btn").textContent = this.galderak[primerRandom].Erantzun1;
+        document.getElementById("option2btn").textContent = this.galderak[primerRandom].ErantzunZuzena;
+        document.getElementById("option3btn").textContent = this.galderak[primerRandom].Erantzun2;
+        break;
+      case 3:
+        document.getElementById("option1btn").textContent = this.galderak[primerRandom].Erantzun2;
+        document.getElementById("option2btn").textContent = this.galderak[primerRandom].Erantzun1;
+        document.getElementById("option3btn").textContent = this.galderak[primerRandom].ErantzunZuzena;
+        break;
+      case 4:
+        document.getElementById("option1btn").textContent = this.galderak[primerRandom].Erantzun2;
+        document.getElementById("option2btn").textContent = this.galderak[primerRandom].ErantzunZuzena;
+        document.getElementById("option3btn").textContent = this.galderak[primerRandom].Erantzun1;
+        break;
+      case 5:
+        document.getElementById("option1btn").textContent = this.galderak[primerRandom].ErantzunZuzena;
+        document.getElementById("option2btn").textContent = this.galderak[primerRandom].Erantzun2;
+        document.getElementById("option3btn").textContent = this.galderak[primerRandom].Erantzun1;
+        break;
+      case 6:
+        document.getElementById("option1btn").textContent = this.galderak[primerRandom].ErantzunZuzena;
+        document.getElementById("option2btn").textContent = this.galderak[primerRandom].Erantzun1;
+        document.getElementById("option3btn").textContent = this.galderak[primerRandom].Erantzun2;
+        break;
+    }
+    console.log(this.ids.length);
+  }
+
+  clickAnswer() {
+    (async () => {
+      await delay(2000);
+
+      this.i = this.ids.length - 1;
+      console.log(this.galderak);
+      for (this.i; this.i < 9; this.i++) {
+        var primerRandom = (Math.floor(Math.random() * this.galderak.length) + 1);
+        if (this.galderak[primerRandom].id in this.ids) {
+          this.i--;
+        }
+        else {
+          this.randomOrderAnswer(primerRandom);
+          break;
+        }
+      }
+    })();
+  }
 
 
 }
