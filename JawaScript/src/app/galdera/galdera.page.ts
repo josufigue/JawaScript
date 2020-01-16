7e27486819e18f62c025a391c2dc386160241032
unknown <Adminpremialocal@HP-PROBOOK-19.uni.lan>
2020-01-16 12:15:20 +0100

    Galderak con loading

--- JawaScript/src/app/galdera/galdera.page.ts
+++ JawaScript/src/app/galdera/galdera.page.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { gald } from '../models/task.interface';
 import { TodogalderakService } from '../services/todogalderak.service'
 import { delay } from 'rxjs/operators';
+import { LoadingController } from '@ionic/angular';
 @Component({
   selector: 'app-galdera',
   templateUrl: './galdera.page.html',
@@ -13,14 +14,19 @@ export class GalderaPage implements OnInit {
   private galderak: gald[];
   private ids = [];
   private i: number;
-  constructor(private galderakService: TodogalderakService) { }
+  loading;
+  constructor(private galderakService: TodogalderakService, private loadingController: LoadingController) { }
 
   ngOnInit() {
     this.galderakService.getAllGalderak().subscribe(res => {
       this.galderak = res;
     });
     (async () => {
-      await this.delay(2000);
+      this.loading = await this.loadingController.create({
+        message: 'Loading...'
+      });
+      await delay(3000);
+      await this.loading.present();
 
       var primerRandom = (Math.floor(Math.random() * this.galderak.length) + 1);
       await this.randomOrderAnswer(primerRandom);
@@ -35,7 +41,8 @@ export class GalderaPage implements OnInit {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
 
-  randomOrderAnswer(primerRandom) {
+  async randomOrderAnswer(primerRandom) {
+    await this.loading.present();
     this.ids.push(this.galderak[primerRandom].id);
     console.log(this.ids);
     document.getElementById("galderaP").textContent = this.galderak[primerRandom].Galdera;
@@ -71,18 +78,20 @@ export class GalderaPage implements OnInit {
         document.getElementById("option3btn").textContent = this.galderak[primerRandom].Erantzun2;
         break;
     }
+    await this.loading.dismiss();
     console.log(this.ids.length);
   }
 
   clickAnswer() {
     (async () => {
-      await delay(2000);
+      await this.loading.present();
+      
 
       this.i = this.ids.length - 1;
       console.log(this.galderak);
       for (this.i; this.i < 9; this.i++) {
         var primerRandom = (Math.floor(Math.random() * this.galderak.length) + 1);
-        if (this.galderak[primerRandom].id in this.ids) {
+        if (this.ids.includes(this.galderak[primerRandom].id)) {
           this.i--;
         }
         else {
