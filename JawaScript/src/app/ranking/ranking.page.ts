c1c47ea4e2cebae62695d7ad4bcb489ce83dd1f4
Ibon Felipe Mora <ibonfelipe99@gmail.com>
2020-01-24 10:29:03 +0100

    mi ranking

--- JawaScript/src/app/ranking/ranking.page.ts
+++ JawaScript/src/app/ranking/ranking.page.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { rankingTask } from '../models/task.interface';
 import { TodorankingService } from '../services/todoranking.service';
 import { Subscription } from 'rxjs';
+import * as firebase from 'firebase/app';
 @Component({
   selector: 'app-ranking',
   templateUrl: './ranking.page.html',
@@ -13,11 +14,22 @@ export class RankingPage {
   ranking: rankingTask[];
   subscription: Subscription = new Subscription();
 
+  miranking;
+  minombre;
+  mispuntos;
+
   constructor(private rankingService: TodorankingService) { }
 
   ionViewWillEnter(){
     this.subscription = this.rankingService.getAllRanking().subscribe(res => {
       this.ranking = res;
+      for(var i=0; i<this.ranking.length; i++){
+        if(firebase.auth().currentUser.uid == this.ranking[i].erabiltzaileId){
+          this.miranking = i+1;
+          this.minombre = this.ranking[i].Izena;
+          this.mispuntos = this.ranking[i].Puntuazioa
+        }
+      }
     });
   }
   formatNumber(i) {
