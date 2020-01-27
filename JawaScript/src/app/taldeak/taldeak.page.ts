dd11a7d77b27df27d38095f6ee8b28a20a904685
Ibon Felipe Mora <ibonfelipe99@gmail.com>
2020-01-27 12:38:55 +0100

    taldeak empezado

--- JawaScript/src/app/taldeak/taldeak.page.ts
+++ JawaScript/src/app/taldeak/taldeak.page.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { taldea } from '../models/task.interface';
+import { TaldeakService } from '../services/taldeak.service';
+import { Subscription } from 'rxjs';
+import { auth } from 'firebase';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-taldeak',
   templateUrl: './taldeak.page.html',
   styleUrls: ['./taldeak.page.scss'],
 })
-export class TaldeakPage implements OnInit {
+export class TaldeakPage {
+  private taldea: taldea[];
+  taldeaitem: taldea = {
+    izena: '',
+    sortzailea: '',
+    partaideak: []
+  };
+  misgrupos: taldea[] = [];
+  subscription: Subscription = new Subscription();
+  constructor(private taldeaservice: TaldeakService) {
 
-  constructor() { }
-
-  ngOnInit() {
   }
-
+  ionViewWillEnter() {
+    this.subscription = this.taldeaservice.getAlltaldeak().subscribe(res => {
+      this.taldea = res;
+      for (var i=0; i < this.taldea.length; i++) {
+        this.taldeaitem = this.taldea[i];
+        for (var j=0; j < this.taldeaitem.partaideak.length; j++) {
+          if(firebase.auth().currentUser.email == this.taldeaitem.partaideak[j]){
+           this.misgrupos.push(this.taldeaitem)
+           console.log(this.misgrupos)
+          }
+        }
+      }
+    });
+  }
 }
