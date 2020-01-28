fe43b061851a81038d905de75619983a0fc27c2c
Ibon Felipe Mora <ibonfelipe99@gmail.com>
2020-01-28 11:46:05 +0100

    no van los taldes de mierda

--- JawaScript/src/app/taldeak/taldeak.page.ts
+++ JawaScript/src/app/taldeak/taldeak.page.ts
@@ -17,23 +17,39 @@ export class TaldeakPage {
     sortzailea: '',
     partaideak: []
   };
+  algo: any;
+  cosa: any;
   misgrupos: taldea[] = [];
   subscription: Subscription = new Subscription();
+  subscription1: Subscription = new Subscription();
   constructor(private taldeaservice: TaldeakService) {
 
   }
   ionViewWillEnter() {
     this.subscription = this.taldeaservice.getAlltaldeak().subscribe(res => {
-      this.taldea = res;
-      for (var i=0; i < this.taldea.length; i++) {
-        this.taldeaitem = this.taldea[i];
-        for (var j=0; j < this.taldeaitem.partaideak.length; j++) {
-          if(firebase.auth().currentUser.email == this.taldeaitem.partaideak[j]){
-           this.misgrupos.push(this.taldeaitem)
-           console.log(this.misgrupos)
+      this.algo = res;
+      for(var i=0; i<this.algo.length; i++){
+        for(var j=0; j<this.algo[i].length.partaideak.length; j++){
+        // for(var j=0; j<this.algo[i].partaideak.length; j++)
+        console.log('hola '+JSON.stringify(this.algo[i].partaideak[j]))
+        // this.subscription1 = this.taldeaservice.gettaldeak(JSON.stringify(this.algo[i].partaideak)).subscribe(res => {
+        //   this.cosa = res;
+          if(JSON.stringify(this.algo[i].partaideak.some(e => e.Id === firebase.auth().currentUser.email))){
+            // this.misgrupos.push(this.cosa)
+            console.log('nooooooooooooooooooooooooooooooooo')
           }
+          console.log(this.algo)
         }
       }
+      
+      // for (var i=0; i < this.taldea.length; i++) {
+      //   for (var j=0; j < this.taldea[i].partaideak.length; j++) {
+      //     if(firebase.auth().currentUser.email == this.taldea[i].partaideak[j]){
+      //      this.misgrupos.push(this.taldea[i])
+      //      console.log(this.misgrupos)
+      //     }
+      //   }
+      // }
     });
   }
 }
