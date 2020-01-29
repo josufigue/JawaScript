4f197662b4a3e78d45ad3d0bafab2bed575ea07f
Ibon Felipe Mora <ibonfelipe99@gmail.com>
2020-01-29 11:04:23 +0100

    los grupos se ven lo prometo(no es la mejor solucion pero funciona)

--- JawaScript/src/app/taldeak/taldeak.page.ts
+++ JawaScript/src/app/taldeak/taldeak.page.ts
@@ -23,7 +23,7 @@ export class TaldeakPage {
   subscription: Subscription = new Subscription();
   subscription1: Subscription = new Subscription();
 
-  i;j;
+  i;j;h=0;
 
   constructor(private taldeaservice: TaldeakService) {
 
@@ -34,19 +34,27 @@ export class TaldeakPage {
       console.log(this.taldeakitem);
       console.log("User: "+ firebase.auth().currentUser.email);
 
-
+      console.log(this.taldeakitem.length)
       for(this.i=0; this.i<this.taldeakitem.length; this.i++){
+        this.taldeak=[]
+        console.log('i='+this.i)
         this.subscription1 = this.taldeaservice.getTaldeak(this.taldeakitem[this.i].izena).subscribe(res2 => {
           this.taldeak = res2;
+          console.log('i='+this.i)
           console.log(this.taldeak);
+          console.log(this.taldeak.length)
           for(this.j=0; this.j<this.taldeak.length; this.j++){
+          console.log('j='+this.j);
             console.log("ID: "+this.taldeak[this.j].Id)
             if(firebase.auth().currentUser.email == this.taldeak[this.j].Id){
-              
-              this.misgrupos.push(this.taldeakitem[this.j]);
+              console.log(this.taldeak[this.j].Id)
+              this.misgrupos.push(this.taldeakitem[this.h]);
               console.log(this.misgrupos);
             }
+            else{
+            }
           }
+          this.h++;
         });
       }
     });
